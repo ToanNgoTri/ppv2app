1e980a536dcee666737dfd6ee89acac03f1d84f5
toan <ngotritoan33@gmail.com>
2026-09-15 21:36:21 +0700

    fix crash

--- android/app/src/main/java/com/ppapp/MainActivity.kt
+++ android/app/src/main/java/com/ppapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.ppapp
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory
 
 class MainActivity : ReactActivity() {
 
@@ -19,4 +21,18 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  /**
+   * Bắt buộc theo hướng dẫn của react-native-screens.
+   *
+   * Khi lâu không mở app, Android sẽ kill process để lấy lại RAM nhưng vẫn giữ
+   * savedInstanceState của Activity. Lúc mở lại, FragmentManager khôi phục các
+   * fragment cũ của react-native-screens trong khi cây JS chưa tồn tại -> crash
+   * native ngay lúc khởi động. RNScreensFragmentFactory xử lý việc khôi phục này
+   * an toàn (gỡ bỏ các fragment mồ côi thay vì dựng lại sai).
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+    super.onCreate(savedInstanceState)
+  }
 }
